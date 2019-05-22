5b557facb164b8fcc5d9719a29a7aec28d28326f
Trent Moss <trentmoss05@gmail.com>
2019-05-22 01:31:06 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -9,6 +9,7 @@ class Keypad extends React.Component {
       <div>
         <input 
           type="password"
-          onKeyUp={this.handleInputPassword})
+          onKeyUp={this.handleInputPassword}
+          )
   }
 }
